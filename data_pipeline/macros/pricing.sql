4149e5b3d51399120fa291f32b3c626476a1b173
Derrick Wafula Simiyu <66744190+D-errick@users.noreply.github.com>
2025-08-20 18:09:18 +0300

    Delete data_pipeline directory

--- data_pipeline/macros/pricing.sql
+++
@@ -1,3 +0,0 @@
-{% macro discounted_amount(extended_price, discount_percentage) %}
-    {{ extended_price }} * (1 - {{ discount_percentage }})
-{% endmacro %}
--- data_pipeline/tests/fct_orders_dates_valid.sql
+++
@@ -1,4 +0,0 @@
-select *
-from {{ ref('fct_orders')}}
-where date(order_date) > current_date()
- or date(order_date) < date('1990-01-01')

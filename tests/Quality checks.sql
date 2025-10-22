3d9b7fc6455def81908f2775dda1d27c62c99373
Derrick Wafula Simiyu <66744190+D-errick@users.noreply.github.com>
2025-10-22 05:03:11 +0300

    Create Quality checks.sql

---
+++ tests/Quality checks.sql
@@ -0,0 +1,35 @@
+-- Check for Nulls or Duplicates in primary key
+-- Expectation: No result
+SELECT *
+FROM silver.crm_prd_info;
+
+SELECT prd_id,
+COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+
+-- Check unwanted spaces
+-- Expectation: No Results
+select prd_nm
+FROM silver.crm_prd_info
+where prd_nm != TRIM(prd_nm);
+
+
+-- Data Standardization and consistency
+SELECT DISTINCT gen
+FROM silver.erp_cust_az12
+
+-- Checking for invalid dates
+SELECT *
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_due_dt OR sls_order_dt > sls_ship_dt;
+
+SELECT DISTINCT cntry
+FROM [DataWarehouse].[silver].[erp_loc_a101]
+ORDER BY cntry
+
+
+SELECT DISTINCT *
+FROM [DataWarehouse].[silver].[erp_loc_a101]
